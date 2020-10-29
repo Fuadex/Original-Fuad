ce1fd1e197247907d04db09cb838a566fc8758a2
Fuadex <Fuadex@gmail.com>
2020-10-30 00:16:45 +1100

    Up
    Up Old

---
+++ new project v2/a1.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>lol</title>
+</head>
+<body>
+<?php
+echo '<b>karol ma kotaaa</b><br>';
+$x = 2;
+$a = 3;
+$c = $x * $a * pi();
+echo $c;
+?>
+</body>
+</html>
---
+++ php/a1.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>lol</title>
+</head>
+<body>
+<?php
+echo '<b>karol ma kotaaa</b>';
+?>
+</body>
+</html>
